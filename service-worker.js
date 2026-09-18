30414489cd82f463cd7534c94f7a60299fe7e24c
dinossaurovelociraptor779-source <dinossaurovelociraptor779@gmail.com>
2026-09-18 03:21:35 -0300

    Force refresh of Cime web shell cache

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
-const CACHE_NAME = 'cime5-shell-v8';
-const SHELL = ['/', '/index.html', '/manifest.json', '/icon-192.png', '/icon-512.png'];
+const CACHE_NAME = 'cime5-shell-v10';
+const SHELL = ['/', '/index.html', '/manifest.json', '/cime-logo.svg'];
 async function putShell(url,response){if(!response||!response.ok||response.type!=='basic')return;const cache=await caches.open(CACHE_NAME);await cache.put(url,response.clone());}
 self.addEventListener('install',event=>{event.waitUntil(caches.open(CACHE_NAME).then(cache=>cache.addAll(SHELL)).then(()=>self.skipWaiting()));});
 self.addEventListener('activate',event=>{event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE_NAME).map(k=>caches.delete(k)))).then(()=>self.clients.claim()));});
